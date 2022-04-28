db9218e3b6ac70f3a5bf8d76399289a66c2d946e
Shikhar Kumar <shikharag50@gmail.com>
2022-04-28 17:16:48 +0530

    my-project

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: "",
+  pusherAppId: "1402484",
+  pusherKey: "1871eea7b4fb3d04e1dd",
+  pusherSecret: "3771f11bed582c54db93",
+  pusherCluster: "ap2",
+  pusherEncrypted: true,
+};
